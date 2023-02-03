81bfe46fd0c89ab4ef1f851f63d939a87e8a4b71
Louis <huynguyennbk@gmail.com>
2023-02-03 19:33:29 +0700

    terraform data sources

---
+++ devops/terraform/aws/data_sources/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+    region = "ap-southeast-1"
+    profile = "louislab"
+}
+
+data "aws_s3_bucket" "bucket" {
+    bucket = "bucket-already-exists"
+}
+
+resource "aws_iam_policy" "my_bucket_policy" {
+    name = "my-bucket-policy"
+
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "${data.aws_s3_bucket.bucket.arn}"
+            ]
+        }
+    ]
+}
+EOF
+}
