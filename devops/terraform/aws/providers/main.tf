a395c74b432a152d52e38ecfeeb83314b9da7ec7
Louis <huynguyennbk@gmail.com>
2023-02-03 18:43:55 +0700

    terraform providers specific version and multiple provider support

---
+++ devops/terraform/aws/providers/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+    region = "ap-southeast-1"
+    profile = "louislab"
+}
+
+provider "aws" {
+    region = "eu-west-2"
+    profile = "louislab"
+    alias = "london"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 3.46"
+    }
+  }
+}
+
+resource "aws_vpc" "ireland_vpc" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "london_vpc" {
+    cidr_block = "10.1.0.0/16"
+    provider = aws.london
+}
