b7f68f5e16b88f87846ebe81d79fbaa4e7ac5a5f
Louis <huynguyennbk@gmail.com>
2023-02-03 20:41:41 +0700

    terraform files

---
+++ devops/terraform/aws/files/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = "ap-southeast-1"
+    profile = "louislab"
+}
+
+resource "aws_iam_policy" "my_bucket_policy" {
+    name = "list-buckets-policy"
+    policy = file("./policy.iam")
+}
