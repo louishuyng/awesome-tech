7443c589d66724aa5fcffb5cda47f26aa51d099e
Louis <huynguyennbk@gmail.com>
2023-02-06 20:31:56 +0700

    terraform null_resource

--- devops/terraform/basic/provisioners/main.tf
+++ devops/terraform/basic/provisioners/main.tf
@@ -98,3 +98,16 @@ resource "aws_instance" "nginx" {
     private_key = file("nginx_key")
   }
 }
+
+
+resource "null_resource" "setup" {
+  provisioner "local-exec" {
+    command = <<CMD
+        ssh -i nginx_key ec2-user@${aws_instance.nginx.public_ip} -o StrictHostKeyChecki\
+        ng=no -o UserKnownHostsFile=/dev/null 'sudo amazon-linux-extras enable nginx1.12; su\
+        do yum -y install nginx; sudo chmod 777 /usr/share/nginx/html/index.html; echo \"Hel\
+        lo from nginx on AWS\" > /usr/share/nginx/html/index.html; sudo systemctl start ngin\
+        x;'
+        CMD
+  }
+}
