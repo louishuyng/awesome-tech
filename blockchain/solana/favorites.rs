d41a94295a4ec039ef253245c0fe240e485dc4f7
Louis Huy Nguyen <huynguyennbk@gmail.com>
2024-11-18 00:23:12 +0700

    Favorites smart contract

---
+++ blockchain/solana/favorites.rs
@@ -0,0 +1,60 @@
+use anchor_lang::prelude::*;
+
+declare_id!("EJhkyEa6CZhqJe34HMh4KBovgqFteCCu6He8PKb3viVr");
+
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
+#[program]
+pub mod favorites {
+    use super::*;
+
+    pub fn set_favorites(
+        context: Context<SetFavorites>,
+        number: u64,
+        color: String,
+        hobbies: Vec<String>,
+    ) -> Result<()> {
+        msg!("Greeting from {}", context.program_id);
+
+        let user_public_key = context.accounts.user.key();
+
+        msg!("User {user_public_key}'s favorite number is {number}, favorite color is {color}, and hobbies are {hobbies:?}");
+
+        context.accounts.favorites.set_inner(Favorites {
+            number,
+            color,
+            hobbies,
+        });
+
+        Ok(())
+    }
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+
+    #[max_len(50)]
+    pub color: String,
+
+    #[max_len(5, 50)]
+    pub hobbies: Vec<String>,
+}
+
+#[derive(Accounts)]
+pub struct SetFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
+        seeds = [b"favorites", user.key().as_ref()],
+        bump
+    )]
+    pub favorites: Account<'info, Favorites>,
+
+    pub system_program: Program<'info, System>,
+}
